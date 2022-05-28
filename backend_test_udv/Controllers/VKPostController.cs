c39359f669934145edd33f608ed1e1c2248c4cc5
Марк <markshubat@gmail.com>
2022-05-28 23:43:24 +0500

    обработал некоторые исключения

--- backend_test_udv/Controllers/VKPostController.cs
+++ backend_test_udv/Controllers/VKPostController.cs
@@ -17,15 +17,29 @@ namespace backend_test_udv.Controllers
     {
         [HttpGet]
         [Route("GetWallPosts")]
-        public Dictionary<char,int> GetPosts(int id = 199373173, int count = 5)
+        public IActionResult GetPosts(int id = 199373173, int count = 5)
         {
+            if (count < 0) return BadRequest("ВВедите неотрицательное количество последних постов");
             var dictionary = new Dictionary<char, int>();
             var PARAMS = $"id={id}&count={count}";
             var request = $"https://api.vk.com/method/wall.get?{PARAMS}&access_token={Startup.vkApiToken}&v=5.131";
-            var textFromPosts = Methods.GetTextFromPostsByReqest(request).Result.ToList();
-            foreach(var elem in textFromPosts)
+            try
             {
-                dictionary = Methods.GetUniqueChars(elem, dictionary);
+                var itemsFromPosts = Methods.GetTextFromPostsByReqest(request).Result.ToList();
+            }
+            catch
+            {
+                return BadRequest("Пользователь не найден или профиль скрыт");
+            }
+            var itemFromPosts = Methods.GetTextFromPostsByReqest(request).Result.ToList();
+            try
+            {
+                if (itemFromPosts.First().owner_id != id) return BadRequest("Пользователь не найден или профиль скрыт");
+            }
+            catch { }
+            foreach (var elem in itemFromPosts)
+            {
+                dictionary = Methods.GetUniqueChars(elem.text, dictionary);
             }
             var post = new Postshistory();
             post.Datetime = DateTime.Now.ToString();
@@ -33,7 +47,7 @@ namespace backend_test_udv.Controllers
             var context = new TestDBContext();
             context.Add(post);
             context.SaveChanges();
-            return dictionary;
+            return Ok(dictionary);
         }
 
         [HttpGet]
@@ -43,37 +57,5 @@ namespace backend_test_udv.Controllers
             var context = new TestDBContext();
             foreach(var elem in context.Postshistories) yield return elem;
         }
-    }
-
-    public static class Methods
-    {
-        public static async Task<List<string>> GetTextFromPostsByReqest(string request)
-        {
-            var client = new HttpClient();
-            var response = await client.GetAsync(request);
-            response.EnsureSuccessStatusCode();
-            var responseBody = await response.Content.ReadAsStringAsync();
-            var post = JsonConvert.DeserializeObject<Rootobject>(responseBody);
-            var list = new List<string>();
-            foreach (var elem in post.response.items)
-            {
-                list.Add(elem.text);
-            }
-            return list;
-        }
-
-        public static Dictionary<char, int> GetUniqueChars(string s, Dictionary<char, int> previous)
-        {
-            var dict = previous;
-            foreach(var elem in s)
-            {
-                if(char.IsLetter(elem) || char.IsDigit(elem))
-                {
-                    if (dict.ContainsKey(char.ToUpper(elem))) dict[char.ToUpper(elem)] += 1;
-                    else dict.Add(char.ToUpper(elem), 1);
-                }
-            }
-            return dict;
-        }
-    }
+    }   
 }
---
+++ backend_test_udv/Methods.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace backend_test_udv.Controllers
+{
+    public static class Methods
+    {
+        public static async Task<List<Item>> GetTextFromPostsByReqest(string request)
+        {
+            var client = new HttpClient();
+            var response = await client.GetAsync(request);
+            response.EnsureSuccessStatusCode();
+            var responseBody = await response.Content.ReadAsStringAsync();
+            var post = JsonConvert.DeserializeObject<Rootobject>(responseBody);
+            var list = new List<Item>();
+            if (post.response == null) return null;        
+            foreach (var elem in post.response.items)
+            {                
+                list.Add(elem);
+            }
+            return list;
+        }
+
+        public static Dictionary<char, int> GetUniqueChars(string s, Dictionary<char, int> previous)
+        {
+            var dict = previous;
+            foreach (var elem in s)
+            {
+                if (char.IsLetter(elem) || char.IsDigit(elem))
+                {
+                    if (dict.ContainsKey(char.ToUpper(elem))) dict[char.ToUpper(elem)] += 1;
+                    else dict.Add(char.ToUpper(elem), 1);
+                }
+            }
+            return dict;
+        }
+    }
+}
